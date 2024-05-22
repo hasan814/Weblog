6a78c8cc8f6e31a8ec0188d6e7f75ee8549378d8
hasan814 <h.mousavi910@gmail.com>
2024-05-22 10:21:28 +0330

    Finished Footer Section

--- src/components/layout/Footer/Footer.jsx
+++ src/components/layout/Footer/Footer.jsx
@@ -1,7 +1,14 @@
 import styles from "./Footer.module.css";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.logo}>Weblog</div>
+      <h1 className={styles.text}>
+        Develeoped by <span>hasan moosavi</span>&copy; all right reserved
+      </h1>
+    </div>
+  );
 };
 
 export default Footer;
